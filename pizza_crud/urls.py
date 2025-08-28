edb2cf125c493dfd8c95ea07574f612a054dc89c
Baikadamov <maxat.baikadamov@narxoz.kz>
2025-08-28 11:30:19 +0500

    static files included

--- pizza_crud/urls.py
+++ pizza_crud/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from ninja import NinjaAPI
 from apps.rest.api import router as rest_router
+from django.conf import settings
+from django.conf.urls.static import static
 
 api = NinjaAPI()
 
@@ -28,3 +30,7 @@ urlpatterns = [
     path('api/', api.urls),
     path('', include('django_prometheus.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
